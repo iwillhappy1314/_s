e3d58adf533a3bcecc0a48c4fb03d8f601d8ae57
李二庆（Amos Lee） <iwillhappy1314@gmail.com>
2020-09-22 17:58:40 +0800

    添加快速购买设置

--- frontend/assets/scripts/woocommerce.js
+++ frontend/assets/scripts/woocommerce.js
@@ -2,18 +2,19 @@
 
     'use strict';
 
-    const body = $('body');
     const _s_wc = {};
 
     _s_wc.init = function() {
         this.ajax_add_to_cart();
         this.sticky_cart();
+        this.quick_shop();
     };
 
     /**
      * Ajax add to cart
      */
     _s_wc.ajax_add_to_cart = function() {
+        const body = $('body');
         body.on('submit', 'form.cart', function(e) {
             const $product_element = $(this).parents('.type-product');
 
@@ -36,47 +37,39 @@
             $add_to_cart_button.removeClass('added not-added');
             $add_to_cart_button.addClass('loading');
 
-            $(document.body).trigger('adding_to_cart', [$add_to_cart_button, data]);
-
             $.ajax({
-                url     : _s_ajax_obj.ajaxurl,
-                data    : data,
-                method  : 'POST',
-                success : function(resp) {
-                    if (!resp) {
-                        return;
-                    }
+                url    : _s_ajax_obj.ajaxurl,
+                data   : data,
+                method : 'POST',
+                success: function(response) {
 
-                    if (resp.error && resp.product_url) {
-                        window.location = resp.product_url;
-                        return;
-                    }
-
-                    $add_to_cart_button.removeClass('loading');
-
-                    const fragments = resp.fragments;
-
-                    if (fragments) {
-                        $.each(fragments, function(key) {
-                            $(key).addClass('updating');
-                        });
-
-                        $.each(fragments, function(key, value) {
-                            $(key).replaceWith(value);
-                        });
-                    }
-
-                    if (0 < resp.notices.indexOf('error')) {
-                        $('body').append(resp.notices);
-                        $add_to_cart_button.addClass('not-added');
+                    if (response.redirect_url) {
+                        window.location = response.redirect_url;
                     } else {
-                        $add_to_cart_button.addClass('added');
+                        $(document.body).trigger('adding_to_cart', [$add_to_cart_button, data]);
+                        $add_to_cart_button.removeClass('loading');
+
+                        const fragments = response.fragments;
+
+                        // 更新购物车
+                        if (fragments) {
+                            $.each(fragments, function(key) {
+                                $(key).addClass('updating');
+                            });
+
+                            $.each(fragments, function(key, value) {
+                                $(key).replaceWith(value);
+                            });
+                        }
+
+                        // show notice
+                        if (0 < response.notices.indexOf('error')) {
+                            $('body').append(response.notices);
+                            $add_to_cart_button.addClass('not-added');
+                        } else {
+                            $add_to_cart_button.addClass('added');
+                        }
                     }
-                },
-                error   : function() {
-                    console.log('error occurred when add product to cart.');
-                },
-                complete: function() {
 
                 },
             });
@@ -85,7 +78,47 @@
         });
 
         body.on('adding_to_cart', function(event, fragments, cart_hash) {
-            $('body').toggleClass('drawer-open');
+            body.toggleClass('drawer-open');
+        });
+    };
+
+    /*
+     * Product Buy Now Button click
+     */
+    _s_wc.quick_shop = function() {
+        $('body').on('click', '.rswc_quick_buy_button', function() {
+            //if (kapee_options.product_add_to_cart_ajax) {
+            //$('.single_add_to_cart_button').addClass('quick-buy-proceed');
+            //}
+
+            const $this = $(this),
+                product_id = $(this).attr('data-product-id'),
+                product_type = $(this).attr('data-product-type'),
+                selected = $('form.cart input#rswc_quick_buy_product_' + product_id),
+                product_form = selected.parents('form.cart'),
+                submit_btn = product_form.find('[type="submit"]'),
+                is_disabled = submit_btn.is(':disabled');
+
+            if (is_disabled) {
+                $('html, body').animate({
+                    scrollTop: submit_btn.offset().top - 200,
+                }, 900);
+            } else {
+                if (!$this.hasClass('disable')) {
+                    product_form.append('<input type="hidden" value="true" name="rswc_quick_buy" />');
+                }
+
+                document.getElementsByClassName('single_add_to_cart_button')[0].click();
+            }
+        });
+
+        $('form.cart').change(function() {
+            const is_submit_disabled = $(this).find('[type="submit"]').is(':disabled');
+            if (is_submit_disabled) {
+                $('.rswc_quick_buy_button').attr('disabled', 'disable');
+            } else {
+                $('.rswc_quick_buy_button').removeAttr('disabled');
+            }
         });
     };
 
--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -482,19 +482,7 @@ if ( ! function_exists('_s_ajax_add_to_cart')) {
      */
     function _s_ajax_add_to_cart()
     {
-        $sku        = '';
-        $product_id = '';
-
-        if (isset($_POST[ 'variation_id' ])) {
-            $sku = $_POST[ 'variation_id' ];
-        }
-        if (isset($_POST[ 'add-to-cart' ])) {
-            $product_id = $_POST[ 'add-to-cart' ];
-        }
-
-        if ( ! isset($sku)) {
-            $sku = $product_id;
-        }
+        $is_quick_buy = isset($_POST[ 'rswc_quick_buy' ]) ? $_POST[ 'rswc_quick_buy' ] : false;
 
         ob_start();
         wc_print_notices();
@@ -504,16 +492,23 @@ if ( ! function_exists('_s_ajax_add_to_cart')) {
         woocommerce_mini_cart();
         $_s_mini_cart = ob_get_clean();
 
-        $data = [
-            'notices'   => $notices,
-            'fragments' => apply_filters(
-                'woocommerce_add_to_cart_fragments',
-                [
-                    'div.widget_shopping_cart_content' => '<div class="widget_shopping_cart_content">' . $_s_mini_cart . '</div>',
-                ]
-            ),
-            'cart_hash' => apply_filters('woocommerce_add_to_cart_hash', WC()->cart->get_cart_for_session() ? md5(json_encode(WC()->cart->get_cart_for_session())) : '', WC()->cart->get_cart_for_session()),
-        ];
+        if ($is_quick_buy) {
+            $data = [
+                'success'      => true,
+                'redirect_url' => wc_get_checkout_url(),
+            ];
+        } else {
+            $data = [
+                'notices'   => $notices,
+                'fragments' => apply_filters(
+                    'woocommerce_add_to_cart_fragments',
+                    [
+                        'div.widget_shopping_cart_content' => '<div class="widget_shopping_cart_content">' . $_s_mini_cart . '</div>',
+                    ]
+                ),
+                'cart_hash' => apply_filters('woocommerce_add_to_cart_hash', WC()->cart->get_cart_for_session() ? md5(json_encode(WC()->cart->get_cart_for_session())) : '', WC()->cart->get_cart_for_session()),
+            ];
+        }
 
         wp_send_json($data);
     }
