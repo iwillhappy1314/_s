3b7ec796d74e32d085b0425db2a9e169e204ca6d
Amos Lee <iwillhappy1314@gmail.com>
2022-05-20 15:37:08 +0800

    代码清理

--- functions.php
+++ functions.php
@@ -1,17 +1,6 @@
 <?php
 
 define('SPACENAME', '1.1.2');
-define( 'NEVE_VERSION', '3.2.2' );
-define( 'NEVE_INC_DIR', trailingslashit( get_template_directory() ) . 'includes/' );
-define( 'NEVE_ASSETS_URL', trailingslashit( get_template_directory_uri() ) . 'assets/' );
-define( 'SPACENAME_MAIN_DIR', get_template_directory() . '/' );
-
-if ( ! defined( 'NEVE_DEBUG' ) ) {
-    define( 'NEVE_DEBUG', false );
-}
-
-define( 'NEVE_NEW_DYNAMIC_STYLE', true );
-
 /**
  * _s functions and definitions
  *
@@ -19,7 +8,6 @@ define( 'NEVE_NEW_DYNAMIC_STYLE', true );
  *
  * @package _s
  */
-
 require_once(get_theme_file_path('vendor/autoload.php'));
 
 
@@ -83,10 +71,3 @@ require_once(get_theme_file_path('includes/cleanup.php'));
 if (class_exists('WooCommerce')) {
     require_once(get_theme_file_path('includes/woocommerce.php'));
 }
-
-// require_once 'includes/migrations.php';
-// require_once 'includes/utilities.php';
-// require_once 'includes/hooks.php';
-// require_once 'includes/sanitize-functions.php';
-// require_once get_theme_file_path('start.php');
-// require_once get_theme_file_path('header-footer-grid/loader.php');
