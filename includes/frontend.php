6c5c69d5cef8479c1d652725269de0e67b331cd5
Amos Lee <iwillhappy1314@gmail.com>
2022-06-24 23:53:43 +0800

    Add style selector to image-box widget

--- includes/frontend.php
+++ includes/frontend.php
@@ -3,7 +3,7 @@
  * @package _s
  */
 
-add_action('wp_enqueue_scripts', '_s_scripts');
+add_action('wp_enqueue_scripts', '_s_scripts', 1);
 
 /**
  * Enqueue scripts and styles.
@@ -16,6 +16,8 @@ function _s_scripts()
     if (is_singular()) {
         wp_enqueue_style('_s-post', _s_assets('dist/styles/post.css'));
     }
+    
+    wp_enqueue_script('wprs-swiper');
 
     wp_enqueue_script('_s-vendors', _s_assets('dist/scripts/vendors.js'), ['jquery'], SPACENAME, true);
     wp_enqueue_script('_s-main', _s_assets('dist/scripts/main.js'), ['jquery'], SPACENAME, true);
@@ -24,6 +26,9 @@ function _s_scripts()
         wp_enqueue_script('comment-reply');
     }
 }
+
+
+ wp_enqueue_script('wprs-swiper');
 
 
 /**
