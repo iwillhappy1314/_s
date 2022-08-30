d1bfed96cf446f4cfdbb6e3f12c5b3f11586da23
Amos Lee <iwillhappy1314@gmail.com>
2022-08-30 18:12:19 +0800

    优化文章列表模版

--- templates/content.php
+++ templates/content.php
@@ -11,14 +11,14 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-    <div class="rs-entry">
+    <div class="grid sm:grid-cols-3 lg:grid-cols-4 gap-4 lg:gap-6 rs-entry">
         <?php if (has_post_thumbnail()) { ?>
-            <div class="rs-entry__image">
-                <?= get_the_post_thumbnail(get_the_id(), 'index-thumbnail'); ?>
+            <div class="aspect aspect-w-4 aspect-h-3 rs-entry__image">
+                <?= get_the_post_thumbnail(get_the_ID(), 'full', ['class' => 'object-cover']); ?>
             </div>
         <?php } ?>
 
-        <div class="rs-entry__body">
+        <div class="sm:col-span-2 lg:col-span-3 rs-entry__body">
             <div class="rs-entry__tag">
                 <?php the_category(); ?>
             </div>
@@ -29,7 +29,7 @@
                 </a>
             </h2>
 
-            <div class="typo rs-entry__desc">
+            <div class="typo">
                 <?php the_excerpt(); ?>
             </div>
 
