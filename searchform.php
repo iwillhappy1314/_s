26e3fed50729cacbdb0fa8812b7c5e1bc76dbb8d
Amos Lee <iwillhappy1314@gmail.com>
2022-08-30 11:10:59 +0800

    add searchform template

---
+++ searchform.php
@@ -0,0 +1,7 @@
+<form role='search' method='get' class='search-form' action='<?= home_url(''); ?>'>
+    <label>
+        <span class='screen-reader-text'>Search for:</span>
+        <input type='search' class='search-field' placeholder='Search …' value='' name='s'>
+    </label>
+    <button type="submit" class='search-submit'><span class='icon-search'></span></button>
+</form>
