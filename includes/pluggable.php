ce4b517ae0c40851cd75d0ff0b530ca0a625c8b7
Amos Lee <iwillhappy1314@gmail.com>
2023-08-14 18:20:16 +0800

    添加回到顶部的按钮

--- includes/pluggable.php
+++ includes/pluggable.php
@@ -13,7 +13,7 @@ add_filter('nav_menu_submenu_css_class', '_s_submenu_css_class');
 add_filter('widget_nav_menu_args', '_s_widget_menu_args');
 add_filter('nav_menu_item_title', '_s_render_shortcode_in_menu_title');
 add_filter('nav_menu_item_args', '_s_append_shortcode_to_menu', 10, 3);
-
+add_filter('wp_footer', '_s_add_to_top_element', 10, 3);
 
 // Code in header and footer
 
@@ -124,7 +124,6 @@ if ( ! function_exists('_s_render_mega_menu_content')) {
 /**
  * Append shortcode content
  */
-add_filter('nav_menu_item_args', '_s_append_shortcode_to_menu', 10, 3);
 function _s_append_shortcode_to_menu($args, $menu_item, $depth)
 {
     $is_mega_menu = get_post_meta($menu_item->ID, 'is_mega_menu', true);
@@ -134,4 +133,19 @@ function _s_append_shortcode_to_menu($args, $menu_item, $depth)
     }
 
     return $args;
+}
+
+
+/**
+ * 添加回到顶部的按钮
+ *
+ * @return void
+ */
+function _s_add_to_top_element()
+{
+    ?>
+    <div class="cursor-pointer rs-back-to-top fixed h-10 w-10 right-6 bottom-6 z-50 border border-solid border-gray-300 inline-flex justify-center items-center hover:border-primary hover:bg-primary group" data-offset="250" data-speed="300" data-easing="swing">
+        <i class="icomoon icon-arrow-up text-gray-300 text-2xl group-hover:text-white"></i>
+    </div>
+    <?php
 }
