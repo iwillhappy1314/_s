8cada2b69b4a961b6cbcc5d1226342025beef094
Amos Lee <iwillhappy1314@gmail.com>
2024-06-14 14:41:07 +0800

    发布主题到父级文件夹

--- bin/publish.sh
+++ bin/publish.sh
@@ -8,7 +8,7 @@ if [ -f "$zip_name" ]; then
 fi
 
 if [ "$1" = "production" ]; then
-    zip -r "$zip_name" . -x "frontend/*.yaml" -x "frontend/*.lock" -x "frontend/*.json" -x "frontend/*.js" -x "bin/*" -x ".git/*" -x "frontend/node_modules/*"
+    zip -r "../$zip_name" . -x "frontend/*.yaml" -x "frontend/*.lock" -x "frontend/*.json" -x "frontend/*.js" -x "bin/*" -x ".git/*" -x "frontend/node_modules/*"
 else
-    zip -r "$zip_name" . -x ".git/*" -x "frontend/node_modules/*"
+    zip -r "../$zip_name" . -x ".git/*" -x "frontend/node_modules/*"
 fi
